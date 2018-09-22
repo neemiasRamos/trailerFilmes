0caca1ad55c1fd1d743e82e633fc412cb60ea221
neemiasRamos <neemias.ferreira@fatec.sp.gov.br>
2018-09-22 10:04:55 -0300

    Update media.py

--- media.py
+++ media.py
@@ -1,17 +1,12 @@
 import webbrowser
-#classe Movie
+
+
 class Movie():
-    #construtor
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        #variaveis do objeto
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-    #método do objeto
     def show_trailer(self):
-        #acessa o navegador e abre o link da variavel do objeto self.trailer_youtube_url
         webbrowser.open(self.trailer_youtube_url)
-
-    
